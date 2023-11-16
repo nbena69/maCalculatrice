8d4ed687e77e663aed76adb4afa0c62e60811e39
nbena69 <naelbenaissapro@gmail.com>
2023-11-16 15:23:26 +0100

    RAS

--- src/app/calculette/calculette.component.ts
+++ src/app/calculette/calculette.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-calculette',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './calculette.component.html',
   styleUrl: './calculette.component.css'
+
 })
 export class CalculetteComponent {
+  public resultat: Number = 0;
+
+  public operande1: FormControl = new FormControl('');
+  public operateur: FormControl = new FormControl('');
+  public operande2: FormControl = new FormControl('');
 
 }
+
+
