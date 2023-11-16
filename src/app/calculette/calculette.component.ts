9c2c67c6b3428ecc101796a3eb5727f7b6c5f9bc
nbena69 <naelbenaissapro@gmail.com>
2023-11-16 16:19:54 +0100

    RAS

--- src/app/calculette/calculette.component.ts
+++ src/app/calculette/calculette.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
@@ -11,12 +11,33 @@ import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/
 
 })
 export class CalculetteComponent {
+  public calculatrice: FormGroup;
   public resultat: Number = 0;
 
-  public operande1: FormControl = new FormControl('');
+  public operande1: FormControl = new FormControl<number>(0, [
+    Validators.min(1),
+    Validators.required
+  ]);
   public operateur: FormControl = new FormControl('');
-  public operande2: FormControl = new FormControl('');
+  public operande2: FormControl = new FormControl<number>(0, [
+    Validators.min(1),
+    Validators.required
+  ]);
 
+  constructor() {
+    this.calculatrice = new FormGroup({
+        operande1: this.operande1,
+        operateur: this.operateur,
+        operande2: this.operande2,
+      }
+    );
+  }
+
+  calcul(): void {
+    this.resultat = eval(
+      parseFloat(this.operande1.value) + this.operateur.value + parseFloat(this.operande2.value)
+    );
+  }
 }
 
 
