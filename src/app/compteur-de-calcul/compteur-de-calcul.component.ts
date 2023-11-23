9ab01a97edfba775c23e936ee14b7b7a139d0943
nbena69 <naelbenaissapro@gmail.com>
2023-11-23 14:41:03 +0100

    RAS

---
+++ src/app/compteur-de-calcul/compteur-de-calcul.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-compteur-de-calcul',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './compteur-de-calcul.component.html',
+  styleUrl: './compteur-de-calcul.component.css'
+})
+export class CompteurDeCalculComponent {
+
+}
